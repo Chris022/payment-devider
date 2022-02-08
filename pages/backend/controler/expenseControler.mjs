aa2b1aaad1c0f1d9b2b6334d8e073c2b9e33f65a
unknown <christoph02.cw@gmail.com>
2022-02-08 10:33:06 +0100

    :construction: make backend work with users.json

--- pages/backend/controler/expenseControler.mjs
+++ pages/backend/controler/expenseControler.mjs
@@ -1,53 +1,55 @@
-import {Expense,columns} from "../models/expense.mjs";
-import { objectVerificator,valueVerificator } from "../utils/verification.mjs";
-import { basicFilter,andList,tryOrIgnore,bind,dateFilter } from '../utils/filter.mjs'
-import { sendSuccess,sendError } from '../utils/result.mjs'
+import { Expense, columns } from "../models/expense.mjs";
+import { objectVerificator, valueVerificator } from "../utils/verification.mjs";
+import { basicFilter, andList, tryOrIgnore, bind, dateFilter } from '../utils/filter.mjs'
+import { sendSuccess, sendError } from '../utils/result.mjs'
+
+import users from "./../../../public/users.json"
 
 //------------Validators------------
-let verifyAll           = objectVerificator(columns);
-let verifyOptional      = objectVerificator(["id"],columns);
-let verifyId            = valueVerificator();
+let verifyAll = objectVerificator(columns);
+let verifyOptional = objectVerificator(["id"], columns);
+let verifyId = valueVerificator();
 
 //------------Filters-----------
-let whereNameLike           =   tryOrIgnore(
-                                    basicFilter("name", "like","whereNameLike"));
-let whereCategoryIdEquals   =   tryOrIgnore(
-                                    basicFilter("category_id", "=","whereCategoryIdEquals"));                      
+let whereNameLike = tryOrIgnore(
+    basicFilter("name", "like", "whereNameLike"));
+let whereCategoryIdEquals = tryOrIgnore(
+    basicFilter("category_id", "=", "whereCategoryIdEquals"));
 
-let categoryFilter          =    andList([
-                                        whereNameLike,
-                                        whereCategoryIdEquals
-                                    ]);
+let categoryFilter = andList([
+    whereNameLike,
+    whereCategoryIdEquals
+]);
 
 // /expense Actions
-export function getAllWithFilter(data){
-    if(!data["filter"] || Object.values(data["filter"]).length == 0){
-        return sendSuccess("Succesfully got all Expenses",Expense.all());
+export function getAllWithFilter(data) {
+    if (!data["filter"] || Object.values(data["filter"]).length == 0) {
+        return sendSuccess("Succesfully got all Expenses", Expense.all());
     }
-    let expenses =  Expense.filter(categoryFilter,data["filter"]);
-    return sendSuccess("Succesfully got all Expenses",expenses);
+    let expenses = Expense.filter(categoryFilter, data["filter"]);
+    return sendSuccess("Succesfully got all Expenses", expenses);
 }
 
-export function removeAll(data){
+export function removeAll(data) {
     Expense.deleteAll();
-    return sendSuccess("Succesfully deleted all Expenses",[]);
+    return sendSuccess("Succesfully deleted all Expenses", []);
 }
 
-export function newExpense(data){
+export function newExpense(data) {
     //Pre edit
-    if(data["fullAmount"]){
-        data["amount"] = data["fullAmount"]/2;
-    }else{
-        data["amount"] = data["halfAmount"];
-        
+    if (data["fullAmount"]) {
+        data["amount"] = (data["fullAmount"] / users.length).toFixed(2);
+    } else {
+        data["amount"] = data["halfAmount"].toFixed(2);
+
     }
     delete data.fullAmount;
     delete data.halfAmount;
 
     //verifcation
-    if(verifyAll(data)){
+    if (verifyAll(data)) {
         Expense.insert(data);
-        return sendSuccess("Succesfully created new Expense",[]);
+        return sendSuccess("Succesfully created new Expense", []);
     }
     return sendError("Invalid or not enought body data");
 }
@@ -55,38 +57,38 @@ export function newExpense(data){
 
 // /expense/id Actions
 
-export function getById(id,data){
+export function getById(id, data) {
     //verifcation
-    if(verifyId(id)){
-        let expense =  Expense.find(id);
-        return sendSuccess("Succesfully got Expense",expense);
+    if (verifyId(id)) {
+        let expense = Expense.find(id);
+        return sendSuccess("Succesfully got Expense", expense);
     }
     return sendError("Invalid or not enought body data");
 }
 
-export function editAll(id,data){
+export function editAll(id, data) {
     //verifcation
-    if(verifyAll(data) && verifyId(id)){
-        Expense.update(id,data);
-        return sendSuccess("Succesfully updated Expense",[]);
+    if (verifyAll(data) && verifyId(id)) {
+        Expense.update(id, data);
+        return sendSuccess("Succesfully updated Expense", []);
     }
     return sendError("Invalid or not enought body data");
 }
 
-export function edit(id,data){
+export function edit(id, data) {
     //verifcation
-    if(verifyOptional(data) && verifyId(id)){
-        Expense.update(id,data);
-        return sendSuccess("Succesfully updated Expense",[]);
+    if (verifyOptional(data) && verifyId(id)) {
+        Expense.update(id, data);
+        return sendSuccess("Succesfully updated Expense", []);
     }
     return sendError("Invalid or not enought body data");
 }
 
-export function removeById(id,data){
+export function removeById(id, data) {
     //verifcation
-    if(verifyId(id)){
+    if (verifyId(id)) {
         Expense.delete(id);
-        return sendSuccess("Succesfully deleted Expense",[]);
+        return sendSuccess("Succesfully deleted Expense", []);
     }
     return sendError("Invalid or not enought body data");
 }
